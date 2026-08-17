84c2ee1677f7a3597706112f13abff76b47f199b
Devon <DevonHurteau@gmail.com>
2026-08-16 20:32:16 -0600

    Added type writing animation.

---
+++ src/main.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const typingAnimation = new Typed('#code-typer', {
+        strings: ["Status: Developer · Clearance to Code"],
+        typeSpeed: 70,
+        backSpeed: 40,
+        backDelay: 1000,
+        loop: true,
+    });
+})
